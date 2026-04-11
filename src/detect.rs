60b4b7cb5bdee0ddc923d562cec4567925906d70
xor <xor@morke.org>
2026-04-11 17:00:38 +0100

    add main probe function to detect.rs

--- src/detect.rs
+++ src/detect.rs
@@ -10,4 +10,19 @@ pub struct Env {
     pub pkg_libs:       Vec<String>,
 }
 
+pub fn probe(cfg: &Config) -> Result<Env, BearError> {
+    let compiler     = find_compiler(&cfg.build.compiler)?;
+    let compiler_ver = compiler_version(&compiler)?;
 
+    let (pkg_cflags, pkg_libs) = match &cfg.deps {
+        Some(deps) if !deps.pkgs.is_empty() => pkg_config(&deps.pkgs)?,
+        _ => (vec![], vec![]),
+    };
+
+    Ok(Env {
+        compiler,
+        compiler_ver,
+        pkg_cflags,
+        pkg_libs,
+    })
+}
